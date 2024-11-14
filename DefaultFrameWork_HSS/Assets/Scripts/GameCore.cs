1ca20fda26f0b68ac69799e6e417ddd3acd93d54
seopseop2 <seopseop2@neptunez.co.kr>
2024-11-14 17:02:20 +0900

    1. 기본 프로젝트 생성 및 코드 추가

---
+++ DefaultFrameWork_HSS/Assets/Scripts/GameCore.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using UnityEngine;
+
+namespace HSS 
+{
+    public class GameCore : SingletonMono<GameCore>
+    {
+        public UIManager UIManager { get; private set; } = new UIManager();
+
+
+        public void Init()
+        {
+            // 기본 프로젝트 내부 설정 처리
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+            Application.targetFrameRate = 60;
+            Time.timeScale = 1f;
+            //Screen.sleepTimeout = SleepTimeout.NeverSleep;  // 절전모드 해제
+        }
+    }
+}
---
+++ DefaultFrameWork_HSS/Assets/Scripts/Manager/UIManager.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HSS
+{
+    public class UIManager
+    {
+        public void Init()
+        {
+
+        }
+    }
+}
