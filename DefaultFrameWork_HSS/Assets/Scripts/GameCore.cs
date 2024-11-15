4a822ce3fc1685589ea36042b4cc3dec33c0613b
shtkeh77 <shtkeh77@gmail.com>
2024-11-15 18:31:28 +0900

    1. 오브젝트풀 추가 및 테스트

--- DefaultFrameWork_HSS/Assets/Scripts/GameCore.cs
+++ DefaultFrameWork_HSS/Assets/Scripts/GameCore.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System.Collections;
+using System.Globalization;
 using UnityEngine;
 
 namespace HSS 
@@ -7,6 +8,14 @@ namespace HSS
     {
         public UIManager UIManager { get; private set; } = new UIManager();
 
+        public GameObject objEmptyPrefab;
+
+        public IEnumerator Start()
+        {
+            yield return null;
+
+            Init();
+        }
 
         public void Init()
         {
@@ -16,6 +25,8 @@ namespace HSS
             Time.timeScale = 1f;
 
             //Screen.sleepTimeout = SleepTimeout.NeverSleep;  // 절전모드 해제
+
+            ObjectPool.CreatePool(objEmptyPrefab, 1);
         }
     }
 }
